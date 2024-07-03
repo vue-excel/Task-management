158e3084224deb3df4311f7380503790f5a2b978
vue-excel <xspace781@gmail.com>
2024-07-03 21:43:00 +0530

    updated index.js

--- server/index.js
+++ server/index.js
@@ -12,6 +12,9 @@ app.use(bodyParser.json())
 
 const corsConfig = function(req, res, next) {
     res.header('Access-Control-Allow-Origin', 'http://localhost:8080')
+    res.header('Access-Control-Allow-Credentials', true)
+    res.header('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT')
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization')
     next()
 }
 
@@ -20,5 +23,8 @@ app.use(corsConfig);
 const apiRoutes = require('./routes/api');
 app.use('/api', apiRoutes);
 
+if (config.seedData) {
+    seederService.seedData()
+}
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
