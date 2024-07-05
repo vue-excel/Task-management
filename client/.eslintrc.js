006f0f786f9ef4c8245dcebb3485364b34064d70
vue-excel <xspace781@gmail.com>
2024-07-05 19:31:38 +0530

    Update .eslintrc.js

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -14,16 +14,15 @@ module.exports = {
     'plugin:vue/essential', 
 
     'standard'
-  ],
-  // required to lint *.vue files
+  ]
   plugins: [
     'vue'
   ],
-  // add your custom rules here
+
   rules: {
-    // allow async-await
+
     'generator-star-spacing': 'off',
-    // allow debugger during development
+  
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     "indent": [
       "error",
