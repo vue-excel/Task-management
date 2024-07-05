b9d31b074e5a50ec2452fe88428390f2300504a9
vue-excel <xspace781@gmail.com>
2024-07-05 19:29:07 +0530

    Update index.js

--- client/config/index.js
+++ client/config/index.js
@@ -22,9 +22,7 @@ module.exports = {
     notifyOnErrors: true,
     poll: false, // https://webpack.js.org/configuration/dev-server/#devserver-watchoptions-
 
-    // Use Eslint Loader?
-    // If true, your code will be linted during bundling and
-    // linting errors and warnings will be shown in the console.
+   
     useEslint: true,
     // If true, eslint errors and warnings will also be shown in the error overlay
     // in the browser.
@@ -62,17 +60,11 @@ module.exports = {
     // https://webpack.js.org/configuration/devtool/#production
     devtool: "#source-map",
 
-    // Gzip off by default as many popular static hosts such as
-    // Surge or Netlify already gzip all static assets for you.
-    // Before setting to `true`, make sure to:
-    // npm install --save-dev compression-webpack-plugin
+  
     productionGzip: false,
     productionGzipExtensions: ["js", "css"],
 
-    // Run the build command with an extra argument to
-    // View the bundle analyzer report after build finishes:
-    // `npm run build --report`
-    // Set to `true` or `false` to always turn it on or off
+
     bundleAnalyzerReport: process.env.npm_config_report
   }
 };
