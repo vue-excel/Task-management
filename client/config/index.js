37cb10a0d4ea4cc8cd281d077852fc7987bbc0e6
vue-excel <xspace781@gmail.com>
2024-07-08 19:45:47 +0530

    Update index.js

--- client/config/index.js
+++ client/config/index.js
@@ -1,6 +1,4 @@
 "use strict";
-// Template version: 1.3.1
-// see http://vuejs-templates.github.io/webpack for documentation.
 
 const path = require("path");
 
@@ -15,29 +13,25 @@ module.exports = {
     // },
 
     // Various Dev Server settings
-    host: "localhost", // can be overwritten by process.env.HOST
-    port: 8080, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
+    host: "localhost", 
+    port: 8080, 
     autoOpenBrowser: false,
     errorOverlay: true,
     notifyOnErrors: true,
-    poll: false, // https://webpack.js.org/configuration/dev-server/#devserver-watchoptions-
-
+    poll: false, 
    
     useEslint: true,
-    // If true, eslint errors and warnings will also be shown in the error overlay
-    // in the browser.
+    
     showEslintErrorsInOverlay: false,
 
     /**
      * Source Maps
      */
 
-    // https://webpack.js.org/configuration/devtool/#development
+  
     devtool: "cheap-module-eval-source-map",
 
-    // If you have problems debugging vue-files in devtools,
-    // set this to false - it *may* help
-    // https://vue-loader.vuejs.org/en/options.html#cachebusting
+
     cacheBusting: true,
 
     cssSourceMap: true
